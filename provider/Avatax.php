3fd789117f9ebf6ef37ec417281899526f2412e9
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-12-31 10:43:27 +0200

    Adding Benchmark_Routing\Provider\Avatax

---
+++ provider/Avatax.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Benchmark_Routing\Provider;
+
+use Benchmark_Routing\Kit\Provider\SwaggerAbstract;
+
+class Avatax extends SwaggerAbstract
+{
+	const LOCAL = 'avatax';
+	const URL = 'https://rest.avatax.com/swagger/v2/swagger.json';
+}
